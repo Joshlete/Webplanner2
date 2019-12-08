2776a050759837aa74d25335bd55217504926cbe
Josh <45409937+Joshlete@users.noreply.github.com>
2019-12-07 22:57:07 -0800

    Updated registration page
    Made registration page work, I think. Not sure if email and pass is actually registering into database *shrug*

--- app/client/main.js
+++ app/client/main.js
@@ -7,20 +7,18 @@ Router.route('/', {
   template: 'login'
 });
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Router.route('/register', function () {
+  this.render('register');
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.register.events({
+  'submit form': function(){
+    event.preventDefault();
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+    Accounts.createUser({
+      email: email,
+      password: password
+    });
+  }
 });
